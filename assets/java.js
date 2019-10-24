ba07c5aeaebdd02c00806709607e59e6b30e342b
Th3freeman <thefrman345@gmail.com>
2019-10-23 23:17:38 -0400

    scrapped timer build, will come back to. added js to swap screens when Start Quiz button is clicked

---
+++ assets/java.js
@@ -0,0 +1,23 @@
+var startQuiz = document.getElementById("quizStart");
+
+startQuiz.addEventListener("click", function(event) {
+    console.log ("butts")
+})
+
+
+
+
+var questions = [
+    {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+    },
+    {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+    },
+
+  ];
+  
