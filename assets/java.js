011a093009d74da7631dae6401f23bc519fb12f7
Th3freeman <thefrman345@gmail.com>
2019-10-23 23:43:46 -0400

    began the process of adding functions and loops to link the questions with the quiz page

--- assets/java.js
+++ assets/java.js
@@ -1,23 +1,30 @@
 var startQuiz = document.getElementById("quizStart");
 
-startQuiz.addEventListener("click", function(event) {
-    console.log ("butts")
-})
-
-
-
+startQuiz.addEventListener("click", function (event) {
+    document.getElementById("Home").classList.add("d-none");
+    document.getElementById("quizScreen").classList.remove("d-none");
+});
 
 var questions = [
     {
-      title: "Commonly used data types DO NOT include:",
-      choices: ["strings", "booleans", "alerts", "numbers"],
-      answer: "alerts"
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
     },
     {
-      title: "The condition in an if / else statement is enclosed within ____.",
-      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      answer: "parentheses"
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
     },
 
-  ];
-  
+];
+
+
+var questionDisplay = document.getElementById("quizQuestions");
+var answerDisplay = document.getElementById("quizAnswers");
+
+
+
+
+
+for (var i = 0; i < )
