bda0cebd631aeefffa04effc762c8567ec18b47f
Th3freeman <thefrman345@gmail.com>
2019-10-25 07:12:47 -0400

    added the function 'chosenOption' to display the next question and alert right or wrong answers but cant get it to work yet. also added onclick functionality  to the form in index.html

--- assets/java.js
+++ assets/java.js
@@ -1,9 +1,42 @@
+var questionDisplay = document.getElementById("quizQuestions");
+var answerDisplay = document.getElementById("quizAnswers");
 var startQuiz = document.getElementById("quizStart");
+
 
 startQuiz.addEventListener("click", function (event) {
     document.getElementById("Home").classList.add("d-none");
     document.getElementById("quizScreen").classList.remove("d-none");
+    questionDisplay.textContent = questions[0].title;
+   
+    for (var i = 0; i < questions[0].choices.length; i++) {
+        var multipleChoice = document.getElementById("options" + i);
+        multipleChoice.text = questions[0].choices[i];
+    };
+
 });
+
+function chosenOption(){
+    var answers = document.getElementsByClassName("answers").text
+    for (var i = 0; i < questions.length; i++){
+        for (var x = 0; x < questions[i].choices.length; x++) {
+            var multipleChoice = document.getElementById("options" + i);
+            multipleChoice.text = questions[0].choices[x];
+        };       
+    }
+    
+    if (
+        answers===questions[i].answer
+    ){
+        alert("Correct!!!")
+        
+    }
+    else(
+        alert("Wrong!!!")
+    )
+    ;
+    console.log (document.getElementsByClassName("answers").text)
+};
+
 
 var questions = [
     {
@@ -18,13 +51,3 @@ var questions = [
     },
 
 ];
-
-
-var questionDisplay = document.getElementById("quizQuestions");
-var answerDisplay = document.getElementById("quizAnswers");
-
-
-
-
-
-for (var i = 0; i < )
